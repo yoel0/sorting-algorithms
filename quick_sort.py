08f8bef8aa1b7fb88905fe1956b584e92871fafe
yoel <yoel@macbook-pro.home>
2020-09-18 11:34:06 -0700

    [quickSort] add example

--- quick_sort.py
+++ quick_sort.py
@@ -9,3 +9,24 @@
 #   c) Concat left + pivot + right
 
 nums = [3, 78, 23, 466, 324, 2, 12, 546, 17, 56, 23, 123, 343, 87, 94, 45, 6, 9, 234, 36]
+# left = [3, 23, 2, 12, 17, 23, 6, 9]
+# right = [78, 466, 324, 546, 56, 123, 343, 87, 94, 45, 234]
+
+# left = [3, 23, 2, 12, 17, 23, 6]
+def quick_sort(ls):
+    # Base case
+    if len(ls) <= 1:
+        return ls
+
+    pivot = ls.pop() # 9
+    left = [x for x in ls if x <= pivot]
+    # left = [3, 2]
+    right = [y for y in ls if y > pivot]
+    # right = [23, 12, 17, 23]
+
+    left_sorted = quick_sort(left)
+    right_sorted = quick_sort(right)
+    print('Result', left_sorted + [pivot] + right_sorted)
+    return left_sorted + [pivot] + right_sorted
+
+quick_sort(nums)
